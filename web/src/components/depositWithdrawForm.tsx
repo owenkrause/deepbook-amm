7f2f5025b4482b7fb5812d62dcd2ef810a0e856d
owenkrause <owenjkrause@gmail.com>
2025-06-11 03:55:49 -0700

    refactor: rename component

--- web/src/components/depositWithdrawForm.tsx
+++ web/src/components/depositWithdrawForm.tsx
@@ -25,13 +25,7 @@ const formSchema = z.object({
     .positive("Amount must be greater than zero."),
 })
 
-const priceIds = [
-  "0x23d7315113f5b1d3ba7a83604c44b94d79f4fd69af77f804fc7f920a6dc65744", // SUI / USD
-  "0x29bdd5248234e33bd93d3b81100b5fa32eaa5997843847e2c2cb16d7c6d9f7ff", // DEEP / USD
-  "0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a", // USDC / USD
-];
-
-export const DepositWithdrawForm = ({ packageId, vaultId }: { packageId: string, vaultId: string }) => {
+export const DepositWithdrawForm = ({ packageId, vaultId, priceIds }: { packageId: string, vaultId: string, priceIds: string[] }) => {
   const currentAccount = useCurrentAccount();
   const { mutate: signAndExecute } = useSignAndExecuteTransaction();
   const { isRegistered, loading: registrationLoading, error: registrationError } = useRegistrationStatus()
