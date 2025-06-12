7d08742207b9d7fdbc60df348d9df387eb2c9464
owenkrause <owenjkrause@gmail.com>
2025-06-11 19:46:16 -0700

    moved parsing to hook

--- web/src/hooks/useVaultBalance.ts
+++ web/src/hooks/useVaultBalance.ts
@@ -1,23 +1,40 @@
+import { useMemo } from "react";
 import { Transaction } from "@mysten/sui/transactions";
 import { useSuiClientQuery } from "@mysten/dapp-kit";
+import { bcs } from "@mysten/sui/bcs";
 
-export function useVaultBalance(packageID: string, vaultID: string, coinType: string) {
-
+export function useVaultBalance(ammPackageId: string, tokenPackageId: string, vaultId: string, coinType: string) {
   const tx = new Transaction();
     
   tx.moveCall({
-    target: `${packageID}::mm_vault::get_balance`,
+    target: `${ammPackageId}::mm_vault::get_vault_balance`,
     typeArguments: [
-      `0xd6cba7469263e9eb6e59a21a724274c70424127dc058c2fc2ba154196d827f82::drip::DRIP`,
+      `${tokenPackageId}::drip::DRIP`,
       coinType
     ],
-    arguments: [tx.object(vaultID)]
+    arguments: [tx.object(vaultId)]
   });
 
-  const { data } = useSuiClientQuery("devInspectTransactionBlock", {
+  const { data, isLoading, error } = useSuiClientQuery("devInspectTransactionBlock", {
     transactionBlock: tx,
-    sender: "0x0bbc599ae81e48d5e67a42c90592109b9ab162d66ec1edef2b305f50015c6228"
+    sender: "0x44e12ed495a913b594b5b73c5358b6a6516d4e3742f7a0dcdec12053b6b0aced"
   });
-  
-  return data;
+
+  const balance = useMemo(() => {
+    if (!data?.results?.[0]?.returnValues?.[0]) {
+      return null;
+    }
+    
+    const returnValue = data.results[0].returnValues[0];
+    
+    if (returnValue[1] === "u64" && Array.isArray(returnValue[0])) {
+      const bytes = new Uint8Array(returnValue[0]);
+      const balanceString = bcs.u64().parse(bytes);
+      return Number(balanceString);
+    }
+
+    return 0;
+  }, [data])
+
+  return { balance, isLoading, error };
 };
