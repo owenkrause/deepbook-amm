9c5d37d960805bef85119600b22a5e97394c8bb9
owenkrause <owenjkrause@gmail.com>
2025-06-11 19:47:20 -0700

    fix: finished implementation

--- web/src/hooks/useVaultTVL.ts
+++ web/src/hooks/useVaultTVL.ts
@@ -1,23 +1,35 @@
 import { Transaction } from "@mysten/sui/transactions";
 import { useSuiClientQuery } from "@mysten/dapp-kit";
 
-export function useVaultTVL(packageID: string, vaultID: string, coinType: string) {
+export function useVaultTVL(ammPackageId: string, tokenPackageId: string, vaultId: string, priceInfoObjectIds: string[]) {
+  const enabled = Boolean(ammPackageId && vaultId && priceInfoObjectIds?.length >= 3);
 
   const tx = new Transaction();
     
-  tx.moveCall({
-    target: `${packageID}::mm_vault::get_total_value`,
-    typeArguments: [
-      `0xd6cba7469263e9eb6e59a21a724274c70424127dc058c2fc2ba154196d827f82::drip::DRIP`,
-      coinType
-    ],
-    arguments: [tx.object(vaultID)]
-  });
+  if (enabled) {
+    tx.moveCall({
+      target: `${ammPackageId}::mm_vault::get_total_value`,
+      typeArguments: [
+        `${tokenPackageId}::drip::DRIP`,
+      ],
+      arguments: [
+        tx.object(vaultId),
+        tx.object(priceInfoObjectIds[2]),
+        tx.object(priceInfoObjectIds[1]),
+        tx.object(priceInfoObjectIds[0]),
+        tx.object("0x6"),
+      ]
+    });
+  }
 
-  const { data } = useSuiClientQuery("devInspectTransactionBlock", {
+  const { data, isLoading, error } = useSuiClientQuery("devInspectTransactionBlock", {
     transactionBlock: tx,
-    sender: "0x0bbc599ae81e48d5e67a42c90592109b9ab162d66ec1edef2b305f50015c6228"
+    sender: "0x44e12ed495a913b594b5b73c5358b6a6516d4e3742f7a0dcdec12053b6b0aced"
   });
   
-  return data;
-};
+  return {
+    data,
+    loading: isLoading,
+    error: error,
+  };
+}
