b9e2f5bb7840e91baf54ecadb7bd32d6d2591068
owenkrause <owenjkrause@gmail.com>
2025-06-13 13:44:43 -0700

    reset config

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["circle.com", "images.deepbook.tech"],
-    dangerouslyAllowSVG: true
-  }
+  
 };
 
 export default nextConfig;
